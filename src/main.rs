e2e570c7624a82440733e99104e439cd7b6584a3
Yuma Takamura <s1729686@stud.uni-frankfurt.de>
2026-02-20 18:01:45 +0100

    try variables type

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
 fn main() {
-    println!("Hello, world!");
+    // 1. learn basic function-structure and variables types.
+    let a : u8 = 26;
+    let b : u8 = 15;
+    let sum: u8 = a + b;
+
+    let x : u8 = 20;
+    let y : u32 = 19;
+    let sub: u8 = x - y as u8; 
+
+    let i : i8 = 12 ;
+    let j : i8 = -14;
+    let sub2: i8 = i -j;
+
+    println!("{} + {} = {}", a, b, sum);
+    println!("{} - {} = {}", x, y, sub);
+    println!("{} - {} = {}", i, j, sub2);
 }
