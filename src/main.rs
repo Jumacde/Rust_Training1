4cec9109295782c6202f17c6757dd4786000f498
Yuma Takamura <s1729686@stud.uni-frankfurt.de>
2026-02-20 21:39:17 +0100

    main

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,10 @@ mod tryList;
 mod tryHashMap;
 
 fn main( ) {
+    println!("<< try calculations. >>");
     calcMethod::testType();
+    println!("<< try Lists. >>");
     tryList::testList();
+    println!("<< try HashMap. >>");
     tryHashMap:: testHashmap();
 }
