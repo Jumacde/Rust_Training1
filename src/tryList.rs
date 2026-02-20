ac21373f65df6ed0894873dfe4ee9a3e9fe27c25
Yuma Takamura <s1729686@stud.uni-frankfurt.de>
2026-02-20 19:41:00 +0100

    basic list

--- src/tryList.rs
+++ src/tryList.rs
@@ -1,5 +1,11 @@
 pub fn testList() {
 
+    let list1 : [i32; 2] = [2, 4];
+    let list2 : [&str; 3] = ["hello", "world", "rust"];
+    println!("list1 is {:?}", list1);
+    println!("the index 0 of List1 is:{} ", list1[0]) ;
+    println!("list2 is {:?}", list2);
+    println!("the index 1 of List2 is:{} ", list2[1]) ;
 
-    
+
 }
