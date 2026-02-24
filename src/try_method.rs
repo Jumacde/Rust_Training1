a4339fc99671db6180fbfe808bebf921eff53e99
Yuma Takamura <s1729686@stud.uni-frankfurt.de>
2026-02-24 17:17:06 +0100

    Commited

--- src/try_method.rs
+++ src/try_method.rs
@@ -1,4 +1,8 @@
 pub fn test_method() {
     let s = String::from("hello world");
+    let mut h = String::from("hello world!");
     println!("{}, {}" , s, s.len());
+    println!("{}",h);
+    h.push_str("A");
+    println!("{}", h);
 }
